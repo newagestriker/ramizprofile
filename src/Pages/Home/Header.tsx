f289573bca349fdbbd928c52dedd894b47ee1cda
NewageStriker <newagestriker@gmail.com>
2020-10-03 18:19:15 +0530

    Complete header section

--- src/Pages/Home/Header.tsx
+++ src/Pages/Home/Header.tsx
@@ -1,4 +1,5 @@
 import * as React from 'react';
+import MyImage from '../../images/mypic.jpg'
 
 export interface HeaderProps {
 
@@ -6,7 +7,16 @@ export interface HeaderProps {
 
 const Header: React.FunctionComponent<HeaderProps> = () => {
     return (<header className='header'>
-        Welcome to my World!!
+        <div className='heading'>
+            <img className='circle-img' src={MyImage} alt='Sk Ramizuddin' />
+            <div className='text-box'>
+                <h1 className='heading-primary'>
+                    <span className='heading-primary-main'>Hi, I'm Ramiz</span>
+                    <span className='heading-primary-sub'>A Web Developer at day & an Android Developer at night</span>
+                </h1>
+                <a className='btn btn-white btn-animated' href='#'>Discover Me</a>
+            </div>
+        </div>
     </header>);
 }
 
