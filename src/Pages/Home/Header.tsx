ad5e158f27b35c7f1314f4d3e003d7540a373dd1
NewageStriker <newagestriker@gmail.com>
2020-10-19 01:14:50 +0530

    Add scss support

--- src/Pages/Home/Header.tsx
+++ src/Pages/Home/Header.tsx
@@ -7,14 +7,14 @@ export interface HeaderProps {
 
 const Header: React.FunctionComponent<HeaderProps> = () => {
     return (<header className='header'>
-        <div className='heading'>
-            <img className='circle-img' src={MyImage} alt='Sk Ramizuddin' />
-            <div className='text-box'>
-                <h1 className='heading-primary'>
-                    <span className='heading-primary-main'>Hi, I'm Ramiz</span>
-                    <span className='heading-primary-sub'>Web Developer at day, Android Developer at night</span>
+        <div className='header__heading'>
+            <img className='img--circle' src={MyImage} alt='Sk Ramizuddin' />
+            <div className='header__text-box'>
+                <h1 className='header__heading-primary'>
+                    <span className='header__heading-primary--main'>Hi, I'm Ramiz</span>
+                    <span className='header__heading-primary--sub'>Web Developer at day, Android Developer at night</span>
                 </h1>
-                <a className='btn btn-white btn-animated' href='#'>Get to know me</a>
+                <a className='btn btn--white btn--animated' href='#'>Get to know me</a>
             </div>
         </div>
     </header>);
