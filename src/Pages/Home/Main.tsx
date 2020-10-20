65f88390676e183c38d31d626824667fee106a9c
NewageStriker <newagestriker@gmail.com>
2020-10-21 03:27:18 +0530

    Add photo to main layout

--- src/Pages/Home/Main.tsx
+++ src/Pages/Home/Main.tsx
@@ -1,5 +1,7 @@
-import React from 'react'
-
+import React from 'react';
+import dotnetcore3 from "../../../src/images/dotnetcore3.jpg";
+import nodejs from "../../../src/images/node-js.png";
+import react from "../../../src/images/react-1.png";
 
 export interface MainProps {
 
@@ -9,19 +11,28 @@ const Main: React.FC<MainProps> = () => {
     return (<main>
         <section className="section-about">
             <div className="u-center-text">
-                <h2 className="heading-secondary u-margin-bottom-8">
+                <h2 className="heading-secondary u-margin-bottom-big">
                     I have a very Interesting Bio!!
             </h2>
             </div>
             <div className="row">
                 <div className="col-1-of-2">
-                    <h3 className="heading-tertiary">You will fall in love with technology</h3>
+                    <h3 className="heading-tertiary u-margin-bottom-small">You will fall in love with technology</h3>
                     <p className="paragraph">
                         Lorem ipsum dolor sit amet consectetur adipisicing elit. Ab repellendus eaque non. Quis fugiat fuga nemo corporis rem eligendi beatae, eius aliquid ab vero magni voluptatibus, impedit commodi mollitia reprehenderit!
                     </p>
+                    <h3 className="heading-tertiary u-margin-bottom-small">You will fall in love with technology</h3>
+                    <p className="paragraph">
+                        Lorem ipsum dolor sit amet consectetur adipisicing elit. Ab repellendus eaque non. Quis fugiat fuga nemo corporis rem eligendi beatae, eius aliquid ab vero magni voluptatibus, impedit commodi mollitia reprehenderit!
+                    </p>
+                    <a href="#top" className="btn-text">Learn more &rarr;</a>
                 </div>
                 <div className="col-1-of-2">
-                    Image Composition
+                    <div className="composition">
+                        <img src={nodejs} alt="Node Js" className="composition__photo composition__photo--p1"/>
+                        <img src={react} alt="React" className="composition__photo composition__photo--p2"/>
+                        <img src={dotnetcore3} alt="Dot Net Core" className="composition__photo composition__photo--p3"/>
+                    </div>
                 </div>
             </div>
 
