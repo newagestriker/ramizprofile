740e0ceb64b3b82e02f1deda49f8f117799c0c09
NewageStriker <newagestriker@gmail.com>
2020-10-24 13:10:05 +0530

    update cards

--- src/Pages/Home/Main.tsx
+++ src/Pages/Home/Main.tsx
@@ -94,32 +94,41 @@ const Main: React.FC<MainProps> = () => {
                 </h2>
             </div>
             <div className="row">
-               <div className="col-1-of-3">
-                   <div className="card">
-                       <div className="card__side card__side--front">
-                          <div className="card__picture card__picture--1">
-                          
-                          </div>
-                          <div className="card__heading">
+                <div className="col-1-of-3">
+                    <div className="card">
+                        <div className="card__side card__side--front">
+                            <div className="card__picture card__picture--1">
 
-                          </div>
-                          <div className="card__details">
+                            </div>
+                            <h4 className="card__heading">
+                                <span className="card__heading-span card__heading-span-1">
+                                    The Sea Explorer
+                              </span>
 
-                          </div>
+                            </h4>
+                            <div className="card__details">
+                                <ul>
+                                    <li>3 day tour</li>
+                                    <li>up to 30 people</li>
+                                    <li>2 tour guides</li>
+                                    <li>Sleep in cozy hotels</li>
+                                    <li>Difficulty: easy</li>
+                                </ul>
+                            </div>
+                        </div>
+                        <div className="card__side card__side--back card__side--back-1">
+                            Back
                        </div>
-                       <div className="card__side card__side--back card__side--back-1">
-                           Back
-                       </div>
-                   </div>
-               </div>
-               <div className="col-1-of-3">
+                    </div>
+                </div>
+                <div className="col-1-of-3">
                     Col 1 of 3
                </div>
-               <div className="col-1-of-3">
+                <div className="col-1-of-3">
                     Col 1 of 3
                </div>
-               
-               
+
+
             </div>
         </section>
     </main>);
